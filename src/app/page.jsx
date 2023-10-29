58551a474940019790aeed52e095518bbc3b1377
Mordecai Nathanael Sirait <mordecaisirait@gmail.com>
2023-10-29 16:14:59 +0700

    commit

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,16 +6,16 @@ import SigninPop from "./signin/page";
 export default function Home() {
   return (
     <>
-      <header className="py-2 shadow-md shadow-gray-600">
-        <nav className="flex container mx-auto justify-between items-center ">
-          <Image src={logo} alt="logo" width="100" height="100" />
-          <div>
-            <input type="text" placeholder="Searching" className="rounded-lg" />
+      <header className="md:flex py-2 p-2 shadow-md shadow-gray-600">
+        <nav className="flex container mx-auto justify-between items-center">
+          <Image src={logo} alt="logo" width="100" height="100" className="mb-2 md:mb-0" />
+          <div className="w-full md:w-auto">
+            <input type="text" placeholder="Searching" className="rounded-lg w-full md:w-auto" />
           </div>
           <div>
-            <SigninPop/>
+            <SigninPop />
           </div>
-        </nav>  
+        </nav>
       </header>
       <main className="container mx-auto mt-10 relative">
         <ListMovie/>
