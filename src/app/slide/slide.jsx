bd5b452e894e152b0f50cda53bd3c22f86b31c42
Mordecai Nathanael Sirait <mordecaisirait@gmail.com>
2023-10-30 22:16:26 +0700

    commit

--- src/app/slide/slide.jsx
+++ src/app/slide/slide.jsx
@@ -1,10 +1,7 @@
 "use client";
 
 import { Carousel } from "flowbite-react";
-import Image from "next/image";
 
 export default function Slide() {
-  return <Carousel pauseOnHover>
-    <Image  />
-  </Carousel>;
+  return <Carousel pauseOnHover></Carousel>;
 }
