cf902dc3b2558aecd1dd2a22dcc75462f2944f41
Mordecai Nathanael Sirait <mordecaisirait@gmail.com>
2023-11-03 19:13:03 +0700

    commit

--- src/app/slide/slide.jsx
+++ src/app/slide/slide.jsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import bg1 from "@/assets/bg1.jpg";
 import bg2 from "@/assets/bg2.jpg";
 import bg3 from "@/assets/bg3.jpg";
+import bg4 from "@/assets/bg4.jpg";
 import { Carousel } from "flowbite-react";
 
 export default function Slide() {
@@ -12,11 +13,30 @@ export default function Slide() {
       <Carousel>
         <div className="relative w-full">
           {/* Pembungkus Carousel */}
+
           <div className="relative h-56 overflow-hidden md:h-96">
-            {/* Item 1 */}
             <Image src={bg1} alt="" />
+          </div>
+        </div>
+        <div className="relative w-full">
+          {/* Pembungkus Carousel */}
+
+          <div className="relative h-56 overflow-hidden md:h-96">
             <Image src={bg2} alt="" />
+          </div>
+        </div>
+        <div className="relative w-full">
+          {/* Pembungkus Carousel */}
+
+          <div className="relative h-56 overflow-hidden md:h-96">
             <Image src={bg3} alt="" />
+          </div>
+        </div>
+        <div className="relative w-full">
+          {/* Pembungkus Carousel */}
+
+          <div className="relative h-56 overflow-hidden md:h-96">
+            <Image src={bg4} alt="" />
           </div>
         </div>
       </Carousel>
